b8ae4d2dca5fa8b0a15ea56d35c7019a632e4650
Kukin Nikita <workkukin@gmail.com>
2023-01-02 20:26:53 +0300

    Добавьте файлы проекта.

---
+++ eLibrary/Entities/Book.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eLibrary.Entities
+{
+    internal class Book
+    {
+        private string _name;
+        private int _year;
+        private string _author;
+        private string _condition;
+        private User _owner;
+        private User _currentReader;
+
+        public Book(string name, int year, string author, string condition, User owner)
+        {
+            _name = name;
+            _year = year;
+            _author = author;
+            _condition = condition;
+            _owner = owner;
+        }
+
+        public void SetReader(User reader) 
+        {
+            _currentReader = reader;
+        }
+
+
+    }
+}
---
+++ eLibrary/Entities/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eLibrary.Entities
+{
+    internal class User
+    {
+        private string _firstName;
+        private string _lastName;
+        private string _userName;
+        private string _password;
+
+        public User(string firstName, string lastName, string userName, string password) 
+        {
+            _firstName= firstName;
+            _lastName = lastName;
+            _userName = userName;
+            _password = password;
+        }
+
+        public bool ConfirmPassword(string input) 
+        {
+            if(input.Equals(_password))
+                return true;
+
+            return false;
+        }
+    }
+}
